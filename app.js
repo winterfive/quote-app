b0746235f37071d811dc44833ee98eb196dc443d
Lee <winterfive@users.noreply.github.com>
2019-02-13 12:18:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+import jQuery
+import json
+
+// select a random quote
+// record that quotes index in array
+// select another quote who's index is not in array
+// continue until all quotes are selected
+// clean array and begin again
+
+// With each quote, change color of bg and font
+// Dict of color pairs
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  this.state = {
+    indexArr: []
+  }
+  
+  /* props */
+
