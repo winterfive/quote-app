7c7f7312640ade1066e06ca792c3f25b98c2ddf1
Lee <winterfive@users.noreply.github.com>
2019-02-19 15:11:19 -0500

    Added current React code

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
-/*
-import jQuery
-import json
+var newQuote;
 
-// select a random quote
-// record that quotes index in array
-// select another quote who's index is not in array
-// continue until all quotes are selected
-// clean array and begin again
-
-// With each quote, change color of bg and font
-// Dict of color pairs
-
-
-class App extends React.Component {
+class MyComp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      author: "name",
+      quote: "quote",
+    }
+		
+		this.getNextQuote = this.getNextQuote.bind(this);
+		this.checkForNewQuote = this.checkForNewQuote.bind(this);
+	}
+	
+	getNextQuote() {
+    $.ajax({
+      url: "https://codepen.io/CodeMoo/pen/KJEmeO.js",
+      type:"get",
+      dataType:'json',  
+      success: function(data){
+        newQuote = data;
+      },
+      error: function() {
+        console.log("err");
+      }
+    });
   }
-  this.state = {
-    indexArr: []
+	
+	checkForNewQuote() {
+    if(newQuote.author != prevAuthor) {
+			render();
+		}
   }
-  
-  /* props */
-
+    
+	render() {
+		return (
+			<div id="quote-box">
+				<div id="text">text</div>
+				<div id="author">author</div>
+				<div id="div-buttons">
+					<div id="tweet-quote"><i class="fab fa-twitter-square fa-2x"></i></div>
+					<div id="facebook-quote"><i class="fab fa-facebook-square fa-2x"></i></div>
+					<div id="email-quote"><i class="fas fa-envelope-square fa-2x"></i></div>
+					<div id="new-quote">
+						<button>new quote</button>
+					</div>
+				</div>      
+			</div> 
+		)
+		document.getElementById('app');
+	}
+}
