35c673a715fbdffd3a3bf4e404a37d03e8ab3073
Lee <winterfive@users.noreply.github.com>
2019-02-20 16:27:07 -0500

    Various changes
    - Removed componentDidMount()
    - Rewrote getNextQuote()

--- app.js
+++ app.js
@@ -1,91 +1,78 @@
 let quotes;
+let index = 0;
 let indexArr = [];
-let tweet = "";
-let quotesLen = 0;
 
 $.ajax({
-	url: "https://codepen.io/CodeMoo/pen/KJEmeO.js",
-	type: "get",
-	dataType: "json",
-	success: function(data) {
-		quotes = data;
-		console.log(quotes[21]);
-	},
-	error: function() {
-		console.log("err");
-	}
-});
-
-console.log("got to here1");
-quotesLen = quotes.length;
-console.log("quotesLen is: " + quotesLen);
+      url: "https://codepen.io/CodeMoo/pen/KJEmeO.js",
+      type:"get",
+      dataType:'json',  
+      success: function(data){				
+        quotes = data;
+      },
+      error: function() {
+        console.log("err");
+      }
+    });
 
 class MyComp extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			author: "name",
-			quote: "quote"
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "name",
+      quote: "quote",
+    }
+		
 		this.getNextQuote = this.getNextQuote.bind(this);
 	}
-
-	componentDidMount() {
-		console.log("comp did mount");
-		getNextQuote();
-	}
-
+	
 	getRandomIndex() {
-		return Math.floor(Math.random() * (quotesSize - 1));
+		return Math.floor(Math.random() * (quotes.length - 1));
 	}
-
+	
 	getNextQuote() {
-		if (indexArr.length == quotesSize) {
+		if(indexArr.length == quotes.length) {
 			indexArr = [];
 		}
-
-		let index = getRandomIndex();
-		console.log("index is: " + index);
-
-		if (indexArr.includes(index)) {
-			console.log("index already used");
-			getNextQuote();
+		
+		index = this.getRandomIndex();		
+		
+		//check indexArr for repeated index
+		if(indexArr.includes(index)) {
+			this.getNextQuote();
 		} else {
-			console.log("new index found");
-			indexArr.concat(index);
 			this.setState({
 				author: quotes[index].author,
 				quote: quotes[index].quote
-			});
+			})
+			indexArr.push(index);
+			console.log("index: " + index);
+			console.log("indexArr: " + indexArr);
 		}
-	}
-
+    
+		this.setState({
+			author: quotes[index].author,
+			quote: quotes[index].quote			
+		})
+  }
+    
 	render() {
 		return (
 			<div id="quote-box">
 				<div id="text-div">
-					<div id="text">{this.state.quote}</div>
-					<div id="author">- {this.state.author}</div>
+				<div id="text">{this.state.quote}</div>
+				<div id="author">- {this.state.author}</div>
 				</div>
 				<div id="div-buttons">
-					<div id="tweet-quote">
-						<a href="twitter.com/intent/tweet?text=this.state.quote&20-&20{this.state.author}">
-							<i class="fab fa-twitter-square fa-2x" />
-						</a>
-					</div>
-					<div id="facebook-quote">
-						<i class="fab fa-facebook-square fa-2x" />
-					</div>
-					<div id="email-quote">
-						<i class="fas fa-envelope-square fa-2x" />
-					</div>
+					<div id="tweet-quote"><i class="fab fa-twitter-square fa-2x"></i></div>
+					<div id="facebook-quote"><i class="fab fa-facebook-square fa-2x"></i></div>
+					<div id="email-quote"><i class="fas fa-envelope-square fa-2x"></i></div>
 					<div id="new-quote">
 						<button onClick={this.getNextQuote}>new quote</button>
 					</div>
-				</div>
+				</div>      
 			</div>
-		);
+		)
 	}
 }
 
-ReactDOM.render(<MyComp />, document.getElementById("app"));
+ReactDOM.render(<MyComp />, document.getElementById('app'));
