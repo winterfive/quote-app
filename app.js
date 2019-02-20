ba7239709db5890be34a6ed57f64913c4040013c
Lee <winterfive@users.noreply.github.com>
2019-02-20 16:43:29 -0500

    Fixed <a> element for twitter tweet to pass FCC tests

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ class MyComp extends React.Component {
 				<div id="author">- {this.state.author}</div>
 				</div>
 				<div id="div-buttons">
-					<div id="tweet-quote"><i class="fab fa-twitter-square fa-2x"></i></div>
+					<div><a id="tweet-quote" href="twitter.com/intent/tweet"><i class="fab fa-twitter-square fa-2x"></i></a></div>
 					<div id="facebook-quote"><i class="fab fa-facebook-square fa-2x"></i></div>
 					<div id="email-quote"><i class="fas fa-envelope-square fa-2x"></i></div>
 					<div id="new-quote">
